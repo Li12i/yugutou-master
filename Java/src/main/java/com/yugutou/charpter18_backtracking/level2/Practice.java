844431621ccb245fc6685a277f6d47da078121ae
Li12i <88259782+Li12i@users.noreply.github.com>
2024-04-11 18:22:08 +0800

    回朔

--- Java/src/main/java/com/yugutou/charpter18_backtracking/level2/Practice.java
+++ Java/src/main/java/com/yugutou/charpter18_backtracking/level2/Practice.java
@@ -1,18 +1,19 @@
 package com.yugutou.charpter18_backtracking.level2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Practice {
 
     public static void main(String[] args) {
         int[] candidates = new int[]{2, 3, 6, 7};
 
-        System.out.println(combinationSum(candidates, 7));
+//        System.out.println(combinationSum(candidates, 7));
+        System.out.println(partition("aab"));
     }
 
+    /**
+     * 组合综合问题
+     */
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
 
         List<List<Integer>> res = new LinkedList<>();
@@ -21,7 +22,7 @@ public class Practice {
         // 排序 方便剪枝
         Arrays.sort(candidates);
 
-        dfs1(candidates, target, 0, res, path);
+        dfs(candidates, target, 0, res, path);
         return res;
     }
 
@@ -47,26 +48,45 @@ public class Practice {
 
     }
 
-    public static void dfs1(int[] candidates, int target, int index, List<List<Integer>> res, LinkedList<Integer> path) {
-        // 退出条件
-        if (target < 0) return;
+    /**
+     * 分割回文串
+     */
+    public static List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        Deque<String> deque = new LinkedList<>();
+        // 主要算法
+        backTracing(s, 0, res, deque);
+        return res;
+    }
 
-        path.addLast(candidates[index]);
-        target -= candidates[index];
-
-        if (target == 0) {
-            res.add(new ArrayList<>(path));
+    public static void backTracing(String s, int startIndex, List<List<String>> res, Deque<String> deque) {
+        // 退出条件 起始位置已经遍历到了最后
+        if (startIndex >= s.length()) {
+            res.add(new ArrayList<>(deque));
             return;
         }
 
-        for (int i = index; i < candidates.length; i++) {
-            // 开始遍历递归
-            System.out.println("递归之前 => " + path + "，剩余 = " + (target));
-            dfs1(candidates, (target), i, res, path);
-            System.out.println("递归之后 => " + path + "，剩余 = " + (target));
-        }
+        // 遍历
+        for (int i = startIndex; i < s.length(); i++) {
+            // 判断是否是回文串并 加入队列
+            if (isPalindrome(s, startIndex, i)) {
+                String str = s.substring(startIndex, i + 1);
+                deque.addLast(str);
+            } else {
+                continue;
+            }
 
-        // 回朔
-        path.removeLast();
+            backTracing(s, i + 1, res, deque);
+            // 回朔
+            deque.removeLast();
+        }
     }
+
+    private static boolean isPalindrome(String s, int startIndex, int end) {
+        for (int i = startIndex, j = end; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+        }
+        return true;
+    }
+
 }
